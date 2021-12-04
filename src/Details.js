27decdc16383e691aab708bc5a116583b927b671
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-12-04 15:24:00 +0600

    Made Details a children of ErrorBoundary as a component can only catch error in its children

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { withRouter } from "react-router";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -38,4 +39,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
