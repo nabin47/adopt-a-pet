abd52d55b9e9ca568a32b2ce61e90ed6c3b2e80a
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-12-04 16:04:08 +0600

    Added redirect after an error occured automatically redirect to homepage after 5000ms

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,17 +1,28 @@
 // mostly code from reactjs.org/docs/error-boundaries.html
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
   static getDerivedStateFromError() {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
     console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
+  //   componentDidUpdate() {
+  //     if (this.state.hasError) {
+  //     }
+  //   }
+  // this approach doesn't work because componentDidUpdate cannot be called the very
+  // first time it renders. So using this means we assume no error will occur in
+  // first time, which is not true. Thats why setTimeout is called inside
+  // componentDidCatch.
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           There was an error with this listing. <Link to="/">Click here</Link>{" "}
