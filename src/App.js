7f4e8ef7898cc6f3c33279ee507d98694b90f48c
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-12-04 16:17:44 +0600

    Wrapped the App function with context so every component can access the theme context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import React from "react"; babel imports react automatically if it sees JSX
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 // const App = () => {
 //   return React.createElement("div", {}, [
@@ -30,27 +31,30 @@ import Details from "./Details";
 // Equivalent JSX for the abaove commented portion
 
 const App = () => {
+  const theme = useState("darkblue");
   return (
-    <div>
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <ThemeContext.Provider value={theme}>
+      <div>
+        {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Lucy" animal="Cat" breed="Siamese" />
     <Pet name="Mithu" animal="Bird" breed="Cockatiel" /> */}
-      <Router>
-        <header>
-          <Link to="/">
-            <h1>Adopt me!</h1>
-          </Link>
-        </header>
-        <Switch>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-          <Route path="/">
-            <SearchParams />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
